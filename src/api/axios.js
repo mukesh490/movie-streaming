ec70851a6200d11d62306e0b1710a2c24433a891
Mukesh Yadav <mukeshyadavbca777@gmail.com>
2026-07-23 15:48:02 +0530

    axios file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 console.log("BASE_URL:", import.meta.env.VITE_BASE_URL);
 
-
-
-import axios from "axios";
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
